68bb6a778129c1f9b97bbecce01fe3d6713d2a93
Caleb Ingalsbe <cdingals@oakland.edu>
2016-09-30 17:06:48 -0400

    Created beer song program

---
+++ BeerSong.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+def BeerSong():
+    for i in range(99, 0, -1):
+        print(i, "Bottles of beer on the wall.\n", i, "bottles of beer. \nTake one down, pass it around,", i, "bottles of beer on the wall.\n")
+    print("You've completed this stupid song! Congrats!")
+BeerSong()
+        
+        
+        
